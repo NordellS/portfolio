db30d29742f7a4989db0b7a0bb1f733b06ee31f3
SaraNordell <sara@nordell.me>
2018-10-05 10:47:32 +0200

    Files added

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="Container">
+
+
+      </div>
+    )
+  }
+
+}
+
+export default App
