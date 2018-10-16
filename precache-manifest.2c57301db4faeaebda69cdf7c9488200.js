9cbe32129f4bb664ec6d77bb0f54ac6984806c7a
SaraNordell <sara@nordell.me>
2018-10-16 21:05:33 +0200

    Updates

---
+++ precache-manifest.2c57301db4faeaebda69cdf7c9488200.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "1bf1b5156403fc4620c5",
+    "url": "/portfolio/static/js/main.1bf1b515.chunk.js"
+  },
+  {
+    "revision": "4571c2e27817abc8d133",
+    "url": "/portfolio/static/js/1.4571c2e2.chunk.js"
+  },
+  {
+    "revision": "1bf1b5156403fc4620c5",
+    "url": "/portfolio/static/css/main.2bc2c699.chunk.css"
+  },
+  {
+    "revision": "896d7d418a21cfd35e7c4de0748b06aa",
+    "url": "/portfolio/index.html"
+  }
+];
