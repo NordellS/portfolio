e3b20c562361bca344bd87ef925281cc75f9f6a1
SaraNordell <sara@nordell.me>
2018-10-18 11:21:52 +0200

    added contactInfo

--- src/components/StartPage/aboutMePresentation.js
+++ src/components/StartPage/aboutMePresentation.js
@@ -19,6 +19,10 @@ class AboutMePresentation extends React.Component {
           Pellentesque habitant morbi tristique senectus et netus et
           malesuada fames ac turpis egestas.
           </p>
+          <div className="contactInfo">
+            <p>sara@nordell.me</p>
+            <p>+46 (0)70 237 44 63</p>
+          </div>
           <ToGithubButton />
         </div>
       </div>
